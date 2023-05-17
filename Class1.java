94c0fe5212e5055f5880ed88d9aac666c99a3a37
KapkarPravin <pravin.kapkar26@gmail.com>
2023-05-17 23:38:00 +0530

    m first comment

---
+++ Class1.java
@@ -0,0 +1,32 @@
+package package1;
+
+import java.util.Scanner;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int num=sc.nextInt();
+	
+         	int sum=0;
+			int num1=num;
+			int numOfDigits = String.valueOf(num).length();
+			while(num1>0) {
+				int digit = num1%10;
+				sum+=Math.pow(digit,numOfDigits);
+				num1=num1/10;
+			}
+			
+			if (sum==num) {
+			   System.out.println(num+ " is NarcissisticNumber");	
+			}
+			else {
+				   System.out.println(num+ " is not NarcissisticNumber");	
+
+			}
+	}
+
+	}
